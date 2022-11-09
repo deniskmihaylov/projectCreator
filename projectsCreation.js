7f7dfb3f99cc8ad27854b03112bd0f8fd54135ae
Denis Mihaylov <116539046+deniskmihaylov@users.noreply.github.com>
2022-11-09 20:20:57 +0200

    Add files via upload

---
+++ projectsCreation.js
@@ -0,0 +1,14 @@
+function projectCount(input) {
+    let name = input[0];
+    let pc = input[1];
+    let hrs = pc * 3
+    console.log(`The architect ${name} will need ${hrs} hours to complete ${pc} project/s.`)
+}
+
+projectCount(['Denis', '3'])
+
+
+
+
+
+//"The architect {името на архитекта} will need {необходими часове} hours to complete {брой на проектите} project/s.
